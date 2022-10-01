fcda13786c06dc1f485e97f042b65b7424cec4b7
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-10-01 08:50:37 -0700

    home_page
    update

---
+++ userinterface2/lib/pages/home_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hi, Ali!',
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text('what would you buy today?',
+                      style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    child: Icon(Icons.notifications,
+                    color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+
+              SizedBox(
+                height: 25,
+              ),
+
+
+              //search bar
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.all(12),
+                child: Row(
+                  children: [
+                    Icon(Icons.search,
+                    color: Colors.orange,),
+                    SizedBox(width: 5),
+                    Text('search',
+                    style: TextStyle(
+                      color: Colors.orange,
+                    ),)
+                  ],
+                ),
+              ),
+
+              SizedBox(width: 25),
+
+              //How do you feel?
+
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
