a2af83a6640f61bfaf367f655188a171e6c75e22
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-10-07 03:05:00 -0700

    update
    Add assets/images again

--- dice_roller_game/lib/main.dart
+++ dice_roller_game/lib/main.dart
@@ -1,104 +1,190 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-Random random = new Random();
-
 void main() {
-  runApp(DiceAppSecAB18());
+  runApp(MaterialApp(
+    //below line "debugShowCheckedModeBanner: false," is used to remove debug banner from app
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        title: Text(
+          'Clickable Rolling Dice App',
+          style: TextStyle(
+            color: Colors.yellow,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: DicePage(),
+    ),
+  ));
 }
 
-class DiceAppSecAB18 extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Dice App 2022"),
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  String rolled_dice = 'None';
+  int top_left = 1;
+  int top_right = 6;
+  int bottom_left = 6;
+  int bottom_right = 1;
+
+  Expanded left() {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            rolled_dice = '';
+            rolled_dice = 'Top Left';
+            top_left = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset(
+          'assets/images/Dice $top_left.png',
+          width: 120,
+          height: 120,
+          //color: Colors.black,
         ),
-        body: Dice(),
       ),
     );
   }
-}
 
-class Dice extends StatefulWidget {
-  const Dice({Key? key}) : super(key: key);
+  Expanded right() {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            rolled_dice = '';
+            rolled_dice = 'Top Right';
+            top_right = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset(
+          'assets/images/Dice $top_right.png',
+          width: 120,
+          height: 120,
+          //color: Colors.black,
+        ),
+      ),
+    );
+  }
 
-  @override
-  State<Dice> createState() => _DiceState();
-}
+  Expanded upper() {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            rolled_dice = '';
+            rolled_dice = 'Bottom Left';
+            bottom_left = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset(
+          'assets/images/Dice $bottom_left.png',
+          width: 120,
+          height: 120,
+          //color: Colors.black,
+        ),
+      ),
+    );
+  }
 
-class _DiceState extends State<Dice> {
-  int num = 1;
-  int num2 = 1;
+  Expanded lower() {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            rolled_dice = '';
+            rolled_dice = 'Bottom Right';
+            bottom_right = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset(
+          'assets/images/Dice $bottom_right.png',
+          width: 120,
+          height: 120,
+          //color: Colors.black,
+        ),
+      ),
+    );
+  }
+
+  Container top_dices() {
+    return Container(
+      child: Row(
+        children: [
+          left(),
+          right(),
+        ],
+      ),
+    );
+  }
+
+  Container bottom_dices() {
+    return Container(
+      child: Row(
+        children: [
+          upper(),
+          lower(),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: TextButton(
-            child: Container(
-              color: Colors.greenAccent,
-              child: Center(
-                child: Image(
-                  image: AssetImage("assets/images/Dice $num.png"),
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Rolled Dice: $rolled_dice'.toString(),
+                      style: TextStyle(
+                        color: Colors.red.shade500,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      'Score is: ' +
+                          (top_left + top_right + bottom_left + bottom_right)
+                              .toString(),
+                      style: TextStyle(
+                        color: Colors.green.shade500,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            onPressed: () {
-              setState(() {
-                num = random.nextInt(6)+1;
-              });
-              print(num);
-            },
+              )
+            ],
           ),
         ),
-        Expanded(
-          child: TextButton(
-            child: Container(
-              color: Colors.greenAccent,
-              child: Center(
-                child: Image(
-                  image: AssetImage("assets/images/Dice $num2.png"),
-                ),
-              ),
-            ),
-            onPressed: () {
-              setState(() {
-                num2 = random.nextInt(6)+1;
-              });
-              print(num2);
-            },
-          ),
+        SizedBox(
+          height: 70.0,
         ),
+        top_dices(),
+        SizedBox(
+          height: 70.0,
+        ),
+        bottom_dices(),
       ],
     );
   }
 }
-
-/*
-         if(num==1)
-            num=2;
-          else if (num==2)
-            num=3;
-          else if (num==3)
-            num=4;
-          else if (num==4)
-            num=5;
-          else if (num==5)
-            num=6;
-          else
-            num=1;
- */
-
-/*
-Container(
-          color: Colors.greenAccent,
-          child: Center(
-            child: Image(
-              image: AssetImage("images/dice1.png"),
-            ),
-          ),
-        )
- */
