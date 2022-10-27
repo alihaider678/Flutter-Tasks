41511de5985d9f51c5a7e5ada980e0cd614699c4
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-10-27 20:59:48 +0500

    update

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,94 +1,35 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/provider/question_provider.dart';
+import 'package:flutterquiz/provider/score_provider.dart';
+import 'package:flutterquiz/screen/dashboard.dart';
+import 'package:flutterquiz/util/router.dart';
+import 'package:flutterquiz/util/router_path.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (BuildContext context) => QuestionProvider() ,
+      ),
+      ChangeNotifierProvider(
+        create: (_)=>ScoreProvider(),
+      )
+    ],
+      child: MyApp(),
+    )
+);
+
+}
 
 class MyApp extends StatelessWidget {
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      onGenerateRoute: Routerr.generateRouter,
+      initialRoute: SplashScreen,
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController controller = TextEditingController();
-  int randomNumber = Random().nextInt(100);
-  String message;
-
-  checkAnswer(int answerNumber) {
-    setState(() {
-      if (answerNumber == randomNumber) {
-        message = 'It is $randomNumber, Congratulation!';
-      } else if (answerNumber < randomNumber) {
-        message = 'Incorrect!';
-      } else {
-        message = 'Try smaller number!';
-      }
-      controller.clear();
-    });
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.yellow.shade100,
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        title: Text("My GuessGame App"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'My number is in rage 0 - 50',
-              style: TextStyle(fontSize: 20),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: TextField(
-                controller: controller,
-                decoration: InputDecoration(hintText: 'Enter your guess number'),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            Text(
-              '$message',
-              style: TextStyle(fontSize: 15),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: ElevatedButton(
-                child: Text('Check'),
-                onPressed: () {
-                  checkAnswer(int.parse(controller.text));
-                },
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
-  }
-}
-
