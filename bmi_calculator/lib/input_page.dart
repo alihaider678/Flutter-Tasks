6a266535d072e18f14b133a221ecefa0fb8f2d26
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-10-31 23:45:21 +0500

    Use of Refactored Code
    1. import font_awesome_flutter library
    2. make own class
    3. And etc...

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -20,11 +21,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: RepeatContainerCode(
                     colors: Color(0xFF1D1E33),
+                    CardWidget: RepeatTextAndIconWidgets(
+                      iconData: FontAwesomeIcons.male,
+                      label: 'Male',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: RepeatContainerCode(
                     colors: Color(0xFF1D1E33),
+                    CardWidget: RepeatTextAndIconWidgets(
+                      iconData: FontAwesomeIcons.female,
+                      label: 'Female',
+                    ),
                   ),
                 ),
               ],
@@ -45,7 +54,7 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: RepeatContainerCode(
-                    colors: Colors.blue,
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
               ],
@@ -57,10 +66,37 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({required this.colors});
-  final Color colors;
+class RepeatTextAndIconWidgets extends StatelessWidget {
+  RepeatTextAndIconWidgets({required this.iconData, required this.label});
+  final IconData iconData;
+  final String label;
 
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          size: 85.0,
+        ),
+        SizedBox(height: 15.0,
+        ),
+        Text(label,
+        style: TextStyle(fontSize: 30.0,
+        color: Color(0xFF8D8E98),
+        ),
+        ),
+      ],
+    );
+  }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode({required this.colors, required this.CardWidget});
+  final Color colors;
+  final Widget CardWidget;
 
   @override
   Widget build(BuildContext context) {
