0909eb7f976525256018f252d9fa5c9e8f86d8df
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-11-03 21:02:06 +0500

    Complete UI of Second Screen
    1. Design Screen for Result
    2. place 2 expanded widgets
    3. design widgets, make style variables and successfully run.

--- bmi_calculator/lib/result_file.dart
+++ bmi_calculator/lib/result_file.dart
@@ -1,4 +1,9 @@
+import 'package:bmi_calculator/constant.dart';
+import 'package:bmi_calculator/container_file.dart';
 import 'package:flutter/material.dart';
+import 'constant.dart';
+import 'input_page.dart';
+
 class ResultScreen extends StatelessWidget {
   const ResultScreen({Key? key}) : super(key: key);
 
@@ -6,9 +11,64 @@ class ResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Result'),
+        title: const Text('BMI Result'),
       ),
-      body: Text('Body Data'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+              child: Container(
+            child: const Center(
+              child: Text(
+                'Your Result',
+                style: kTitleStyleS2,
+              ),
+            ),
+          )),
+          Expanded(
+              child: RepeatContainerCode(
+            colors: activeColor,
+            CardWidget: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'Normal',
+                  style: kResultText,
+                ),
+                Text(
+                  '18.3',
+                  style: kBMiTextStyle,
+                ),
+                Text(
+                  'BMI is Low you should have to work more',
+                  textAlign: TextAlign.center,
+                  style: kbodyTextStyle,
+                )
+              ],
+            ),
+            onPressed: () {},
+          ),
+          ),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>InputPage()));
+            },
+            child: Container(
+              child: const Center(
+                  child: Text(
+                    'Recalculate',
+                    style: kLargeButtonStyle,
+                  )),
+              color: const Color(0xFFEB1555),
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
