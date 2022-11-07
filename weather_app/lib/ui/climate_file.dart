6ee76e4a6b9d051ffee2f6affbacf3be8459dbb1
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-11-07 23:42:21 +0500

    Use of Stack Widget
    1. In body, use stack widget for place an image
    2. paste images in directory folder and pub get it.
    3. Successfully place it.

--- weather_app/lib/ui/climate_file.dart
+++ weather_app/lib/ui/climate_file.dart
@@ -21,6 +21,18 @@ class _ClimateState extends State<Climate> {
           )
         ],
       ),
+      body: Stack(
+        children: [
+          Center(
+            child: Image(
+              image: AssetImage('images/umbrella.png'),
+              height: 1200.0,
+              width: 600.0,
+              fit: BoxFit.fill,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
