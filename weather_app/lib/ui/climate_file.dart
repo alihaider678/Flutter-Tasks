3593cc4b27358f92619ef9c1cc95109203592888
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-11-07 23:22:44 +0500

    UI Part 02
    1. Design Scaffold Widgets
    2. Design App Bar with Icon.

--- weather_app/lib/ui/climate_file.dart
+++ weather_app/lib/ui/climate_file.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class Climate extends StatefulWidget {
   const Climate({Key? key}) : super(key: key);
 
@@ -12,6 +10,17 @@ class Climate extends StatefulWidget {
 class _ClimateState extends State<Climate> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ClimateApp'),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+            onPressed: () => print("clicked"),
+            icon: Icon(Icons.menu),
+          )
+        ],
+      ),
+    );
   }
 }
