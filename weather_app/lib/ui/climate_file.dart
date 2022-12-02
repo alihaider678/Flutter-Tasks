69a519bd7a32a5dcbceb515c396d0478016f0998
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-12-02 20:50:08 +0500

    update
    One Screen result

--- weather_app/lib/ui/climate_file.dart
+++ weather_app/lib/ui/climate_file.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import '../utils/api_file.dart' as util;
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class Climate extends StatefulWidget {
-  const Climate({Key? key}) : super(key: key);
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+import '../utils/api_file.dart' as util;
+
+class Climate extends StatefulWidget {
   @override
-  State<Climate> createState() => _ClimateState();
+  _ClimateState createState() => _ClimateState();
 }
 
 class _ClimateState extends State<Climate> {
@@ -16,30 +16,10 @@ class _ClimateState extends State<Climate> {
     Map data = await getWeather(util.apiId, util.defaultCity);
     print(data.toString());
   }
-  @override
-  void initState() {
-    showStuff();
-    super.initState();
-  }
 
   String? _cityEntered;
-  Future _goToNextScreen(BuildContext context) async {
-    Map? results = await Navigator.of(context)
-        .push(MaterialPageRoute<Map>(builder: (BuildContext context) {
-      //change to Map instead of dynamic for this to work
-      return ChangeCity();
-    }));
 
-    if (results != null && results.containsKey('enter')) {
-      setState(() {
-        _cityEntered = results['enter'];
-      });
-
-//      debugPrint("From First screen" + results['enter'].toString());
-
-    }
-  }
-
+  var _cityFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -47,29 +27,22 @@ class _ClimateState extends State<Climate> {
       appBar: AppBar(
         title: Text('ClimateApp'),
         backgroundColor: Colors.red,
-        actions: [
-          IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                _goToNextScreen(context);
-              })
-        ],
       ),
       body: Stack(
-        children: [
+        children: <Widget>[
           Center(
             child: Image(
               image: AssetImage('images/umbrella.png'),
-              height: 1200.0,
+              height: 1250.0,
               width: 600.0,
               fit: BoxFit.fill,
             ),
           ),
           Container(
             alignment: Alignment.topRight,
-            margin: EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            margin: EdgeInsets.fromLTRB(0.0, 150.9, 20.9, 0.0),
             child: Text(
-              _cityEntered == null ? util.defaultCity : _cityEntered ?? "",
+              '${_cityEntered == null ? util.defaultCity : _cityEntered}',
               style: cityStyle(),
             ),
           ),
@@ -80,17 +53,72 @@ class _ClimateState extends State<Climate> {
           ),
           Container(
             margin: EdgeInsets.fromLTRB(30.0, 90.0, 0.0, 0.0),
-            child: updateTempWidget('${_cityEntered == null ? util.defaultCity : _cityEntered}'),
+            child: updateTempWidget(
+                '${_cityEntered == null ? util.defaultCity : _cityEntered}'),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30, right: 30, top: 15),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white),
+                child: TextField(
+                  controller: _cityFieldController,
+                  onSubmitted: (value) {
+                    _cityEntered = value;
+                  },
+                  decoration: InputDecoration(
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _cityEntered = _cityFieldController.text;
+                        });
+                      },
+                      child: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(100)),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          )),
+                    ),
+                    label: Text("Search City"),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.orangeAccent, width: 4.0),
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(30.0),
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.orangeAccent, width: 4.0),
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 
-  Future<Map> getWeather(String apiId, String city) async {
+  Future<Map> getWeather(String appId, String city) async {
     String? apiUrl =
         'http://api.openweathermap.org/data/2.5/weather?q=$city&appid='
         '${util.apiId}&units=imperial';
+    //"http://api.openweathermap.org/data/2.5/weather?q=vehari&appid=ee595cbdb0db0ef5d11b9caf5a8eb1ea&units=metric";
     http.Response? response = await http.get(Uri.parse(apiUrl));
 
     return json.decode(response.body);
@@ -105,23 +133,23 @@ class _ClimateState extends State<Climate> {
             Map? content = snapshot.data;
             return Container(
               margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
-              child: Column(
+              child: new Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  ListTile(
-                    title: Text(
+                  new ListTile(
+                    title: new Text(
                       content!['main']['temp'].toString() + " F",
-                      style: TextStyle(
+                      style: new TextStyle(
                           fontStyle: FontStyle.normal,
                           fontSize: 49.9,
                           color: Colors.white,
                           fontWeight: FontWeight.w500),
                     ),
-                    subtitle: ListTile(
-                      title: Text(
-                        "Humidity: ${content!['main']['humidity'].toString()}\n"
-                        "Min: ${content['main']['temp_min'].toString()} F\n"
-                        "Max: ${content['main']['temp_max'].toString()} F ",
+                    subtitle: new ListTile(
+                      title: new Text(
+                        "Humidity: ${content['main']['humidity'].toString()}\n"
+                            "Min: ${content['main']['temp_min'].toString()} F\n"
+                            "Max: ${content['main']['temp_max'].toString()} F ",
                         style: extraData(),
                       ),
                     ),
@@ -135,6 +163,7 @@ class _ClimateState extends State<Climate> {
         });
   }
 }
+
 class ChangeCity extends StatelessWidget {
   var _cityFieldController = TextEditingController();
   @override
@@ -183,6 +212,7 @@ class ChangeCity extends StatelessWidget {
     );
   }
 }
+
 TextStyle cityStyle() {
   return TextStyle(
     color: Colors.white,
@@ -190,6 +220,7 @@ TextStyle cityStyle() {
     fontStyle: FontStyle.italic,
   );
 }
+
 TextStyle tempStyle() {
   return TextStyle(
       color: Colors.white,
@@ -197,6 +228,7 @@ TextStyle tempStyle() {
       fontWeight: FontWeight.w500,
       fontSize: 49.9);
 }
+
 TextStyle extraData() {
   return TextStyle(
       color: Colors.white70, fontStyle: FontStyle.normal, fontSize: 17.0);
--- weather_app/lib/utils/api_file.dart
+++ weather_app/lib/utils/api_file.dart
@@ -1,2 +1,2 @@
-final apiId = "dad983fb321b0945874078eed9690bff";
+final apiId = "331aa8eb37d044c348df5ff4e3f2a77d";
 final defaultCity = "Vehari";
