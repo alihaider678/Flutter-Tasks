767e23b753f3f1cad6d59ddcfe709a3d6996050e
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2022-12-17 21:47:46 +0500

    update
    1. One line code change

--- party_willingness_management_app/test/widget_test.dart
+++ party_willingness_management_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:party_willingness_management_app/main.dart';
+import 'package:party/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
